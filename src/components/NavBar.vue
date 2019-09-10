4a62f7cb67119c61fb948739c2a57306db0a8ebb
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-09-10 13:37:06 -0300

    mobile menu working well

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,17 +2,17 @@
   <div class="nav-bar nav-bar--mobile">
     <div class="nav-bar__content  nav-bar__content--mobile">
       <img class="nav-bar__logo" src="@/assets/logo-python.svg" alt="">
-      <div class="nav-bar__hamburger">
+      <div class="nav-bar__hamburger" @click="openMobileMenu">
         <div class="hamburger__line"></div>
         <div class="hamburger__line"></div>
         <div class="hamburger__line"></div>
       </div>
-      <div id="nav" class="nav-bar__links">
-        <router-link class="nav-bar__link" to="/">Home</router-link>
-        <router-link class="nav-bar__link" to="/local">Local</router-link>
-        <router-link class="nav-bar__link" to="/key-notes/">Key Notes</router-link>
-        <a class="nav-bar__link" href="https://pythonsul.org/documents/plano_de_patrocinio.pdf" target="_blank">Patrocinar /</a>
-        <a class="nav-bar__link" href="https://pythonsul.org/documents/sponsorship_plan.pdf" target="_blank">Sponsorship</a>
+      <div id="nav" class="nav-bar__links" :class="{'nav-bar__links--mobile': mobile}">
+        <router-link class="js-nav-bar-link nav-bar__link" to="/">Home</router-link>
+        <router-link class="js-nav-bar-link nav-bar__link" to="/local">Local</router-link>
+        <router-link class="js-nav-bar-link nav-bar__link" to="/key-notes/">Key Notes</router-link>
+        <a class="js-nav-bar-link nav-bar__link" href="https://pythonsul.org/documents/plano_de_patrocinio.pdf" target="_blank">Patrocinar /</a>
+        <a class="js-nav-bar-link nav-bar__link" href="https://pythonsul.org/documents/sponsorship_plan.pdf" target="_blank">Sponsorship</a>
       </div>
     </div>
   </div>
@@ -20,7 +20,30 @@
 
 <script>
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+
+  data: () => {
+    return {
+      mobile: false
+    }
+  },
+
+  mounted () {
+    let that = this
+    this.$el
+      .querySelectorAll('.js-nav-bar-link')
+      .forEach((el) => { el.onclick = () => that.hideMobileMenu() })
+  },
+
+  methods: {
+    openMobileMenu () {
+      this.mobile = !this.mobile
+    },
+
+    hideMobileMenu () {
+      this.mobile = false
+    }
+  }
 }
 </script>
 
