c2fbbd325123c25d1ef93897b0ecab07bdeeaef1
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-09-11 12:56:06 -0300

    fix sections angle

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -5,7 +5,7 @@
       <img class="main-banner__curitiba" src="@/assets/PythonSul2019-BotanicGarden.svg" alt=""/>
     </div>
     <div class="main-banner__actions">
-      <a class="btn btn--primary" href="https://www.eventbrite.com.br/e/pythonsul-2019-curitiba-tickets-65624868749" target="_blank">Inscrições</a>
+      <a class="btn btn--primary main-banner__btn-inscricao" href="https://www.eventbrite.com.br/e/pythonsul-2019-curitiba-tickets-65624868749" target="_blank">Inscrições</a>
       <a class="btn btn--rdf-secondary" href="https://speakerfight.com/events/pythonsul-2019-curitiba/" target="_blank">Envie uma palestra</a>
     </div>
   </div>
