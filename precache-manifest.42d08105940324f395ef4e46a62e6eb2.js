e1f7b199db58c101fbffd6e56d9976ddb084cd81
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-09-12 08:48:14 -0300

    deploy

---
+++ precache-manifest.42d08105940324f395ef4e46a62e6eb2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "5029c085151acf7e2bc3",
+    "url": "/pythonsul-2019/js/about.47c0c841.js"
+  },
+  {
+    "revision": "f63aadcd032a1a051632",
+    "url": "/pythonsul-2019/css/app.029fcea4.css"
+  },
+  {
+    "revision": "f63aadcd032a1a051632",
+    "url": "/pythonsul-2019/js/app.b92bf482.js"
+  },
+  {
+    "revision": "1b9b681ae0a6fb3bb293",
+    "url": "/pythonsul-2019/css/chunk-vendors.81e18a23.css"
+  },
+  {
+    "revision": "1b9b681ae0a6fb3bb293",
+    "url": "/pythonsul-2019/js/chunk-vendors.3f083f8f.js"
+  },
+  {
+    "revision": "8f659a94654f5c1ea135fa82795b788b",
+    "url": "/pythonsul-2019/img/LogoTipoPythonSul2.8f659a94.svg"
+  },
+  {
+    "revision": "a205eea353a0e29f8286a2763a50d041",
+    "url": "/pythonsul-2019/img/unicuritiba.a205eea3.jpg"
+  },
+  {
+    "revision": "a5978091a6e6ca94d3d5df89e8d8f893",
+    "url": "/pythonsul-2019/img/unicuritiba.a5978091.png"
+  },
+  {
+    "revision": "59bbc192525c793bd73d444ffb23a866",
+    "url": "/pythonsul-2019/fonts/Flux.59bbc192.woff"
+  },
+  {
+    "revision": "ee7dc64be1db60bbe4db4c87769b6f24",
+    "url": "/pythonsul-2019/img/Flux.ee7dc64b.svg"
+  },
+  {
+    "revision": "01d21412d9986d349b4edbd996b66161",
+    "url": "/pythonsul-2019/fonts/Flux.01d21412.eot"
+  },
+  {
+    "revision": "91a90e09fc61de147e200caa04e63c1d",
+    "url": "/pythonsul-2019/fonts/Flux.91a90e09.otf"
+  },
+  {
+    "revision": "119fd118fd31150b543266f297283f57",
+    "url": "/pythonsul-2019/fonts/Flux.119fd118.ttf"
+  },
+  {
+    "revision": "8999106d7006f53482db7c2ffe75aeae",
+    "url": "/pythonsul-2019/img/araucaria.8999106d.jpg"
+  },
+  {
+    "revision": "642485a632db8d532e1ef80ac9522fb1",
+    "url": "/pythonsul-2019/img/LogoTipoPythonSul.642485a6.svg"
+  },
+  {
+    "revision": "2aec65f47d7d18680454c12f8c1a0107",
+    "url": "/pythonsul-2019/img/logo-python.2aec65f4.svg"
+  },
+  {
+    "revision": "ca26a70f538577445c751941558f3cd0",
+    "url": "/pythonsul-2019/img/melissa_mendonca.ca26a70f.jpg"
+  },
+  {
+    "revision": "79f6afb9feab98acb091721273c0a727",
+    "url": "/pythonsul-2019/img/araucaria.79f6afb9.svg"
+  },
+  {
+    "revision": "cd29336fd69df3770989f2677e102aed",
+    "url": "/pythonsul-2019/img/PythonSul2019-NomedoEvento.cd29336f.svg"
+  },
+  {
+    "revision": "692ebf38d746bad5553713fd41edde77",
+    "url": "/pythonsul-2019/img/pythonsul-logo.692ebf38.png"
+  },
+  {
+    "revision": "682578a99ada237d1b363f760dd3161f",
+    "url": "/pythonsul-2019/img/PythonSul2019-BotanicGarden.682578a9.svg"
+  },
+  {
+    "revision": "f245bb0610a8c90f23979dbdf031f6fb",
+    "url": "/pythonsul-2019/img/pythonsul_icon.f245bb06.png"
+  },
+  {
+    "revision": "d528558fd915c4ae08ea2516fcbb6ba7",
+    "url": "/pythonsul-2019/img/LogoTipoPythonSul1.d528558f.svg"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/pythonsul-2019/img/1024px-Python-logo-notext.svg.067050f6.png"
+  },
+  {
+    "revision": "3b40aaa5c14a7c47eecf02421b6e275b",
+    "url": "/pythonsul-2019/img/PythonSul2019-JardimBotanico.3b40aaa5.svg"
+  },
+  {
+    "revision": "804bbc4e40d110b40f8bc42ba735acfe",
+    "url": "/pythonsul-2019/img/mario_sergio.804bbc4e.jpg"
+  },
+  {
+    "revision": "788c53dd371f12eae93578381f24e48a",
+    "url": "/pythonsul-2019/img/LogoPythonSul.788c53dd.svg"
+  },
+  {
+    "revision": "d479d2509f5255fb4da6a1a6bf2f576e",
+    "url": "/pythonsul-2019/img/thais_viana.d479d250.jpg"
+  },
+  {
+    "revision": "5cddc3f5bd09c3a5e5e946fa60f1fccd",
+    "url": "/pythonsul-2019/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pythonsul-2019/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pythonsul-2019/precache-manifest.42d08105940324f395ef4e46a62e6eb2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pythonsul2019-site-rebuild"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
